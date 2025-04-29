a2b555221910bf59646b781920fe9d1d254e1fa8
nethead21 <junivensaavedra@gmail.com>
2025-04-29 21:34:53 +0800

    Initialized tuple in collection_data_structures.py

---
+++ collection_data_structures.py
@@ -0,0 +1,18 @@
+stock = "AAPL", 123.52, 53.15, 137.98
+stock2 = ("AAPL", 123.52, 53.15, 137.98)
+
+print(f"{stock=}")
+print(f"{stock2=}")
+print(stock[2])
+print(stock[1:3])
+
+a = (42,)
+b = (42,)
+print(f"{a=}")
+print(f"{b=}")
+
+c = (
+    (43, 3.14),
+    (2.732, 3.224),
+)
+print(f"{c=}")
