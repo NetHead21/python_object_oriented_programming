a4735fe8ff562636e03946426eb031504e53f31e
nethead21 <junivensaavedra@gmail.com>
2025-05-19 21:29:14 +0800

    Added Chapter 8 get_pages.py

---
+++ ch8/get_pages.py
@@ -0,0 +1,13 @@
+from urllib.parse import urlparse
+from pathlib import Path
+
+def get_pages(*links: str) -> None:
+    for link in links:
+        url = urlparse(link)
+        name = "index.html" if url.path in ("", "/") else url.path
+        target = Path(url.netloc.replace(".", "_")) / name
+        print(f"Create {target} from {link}")
+
+get_pages()
+get_pages('https://www.archlinux.org')
+get_pages('https://www.archlinux.org', 'https://dusty.phillips.codes', 'https://itmaybeahack.com')
