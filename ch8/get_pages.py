7dc9f30b2274fcac4adf241b994da0638c528d7e
nethead21 <junivensaavedra@gmail.com>
2025-05-20 23:08:34 +0800

    Added get_pages and options in ch8

--- ch8/get_pages.py
+++ ch8/get_pages.py
@@ -1,6 +1,7 @@
 from urllib.parse import urlparse
 from pathlib import Path
 
+
 def get_pages(*links: str) -> None:
     for link in links:
         url = urlparse(link)
@@ -8,6 +9,11 @@ def get_pages(*links: str) -> None:
         target = Path(url.netloc.replace(".", "_")) / name
         print(f"Create {target} from {link}")
 
+
 get_pages()
-get_pages('https://www.archlinux.org')
-get_pages('https://www.archlinux.org', 'https://dusty.phillips.codes', 'https://itmaybeahack.com')
+get_pages("https://www.archlinux.org")
+get_pages(
+    "https://www.archlinux.org",
+    "https://dusty.phillips.codes",
+    "https://itmaybeahack.com",
+)
---
+++ ch8/options.py
@@ -0,0 +1,13 @@
+class Options(dict[str, any]):
+    default_options: dict[str, any] = {
+        "port": 21,
+        "host": "localhost",
+        "username": None,
+        "password": None,
+        "debug": False
+    }
+
+    def __init__(self, **kwargs: any) -> None:
+        super().__init__(self.default_options)
+        self.update(kwargs)
+
