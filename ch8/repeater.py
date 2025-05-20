537d011b9c8cf53e9d26564401d5354f72804487
nethead21 <junivensaavedra@gmail.com>
2025-05-21 02:24:03 +0800

    Added fource method to Class Repeater

---
+++ ch8/repeater.py
@@ -0,0 +1,10 @@
+from .format_time import format_time
+
+class Repeater:
+    def __init__(self) -> None:
+        self.count = 0
+
+    def four(self, timer: float) -> None:
+        self.count += 1
+        format_time(f"Called Four: {self.count}")
+        
