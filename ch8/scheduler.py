840ec9201b517ce8fb8b848ac4a8eecdf5603452
nethead21 <junivensaavedra@gmail.com>
2025-05-21 02:11:19 +0800

    Added enter method for Scheduler class.

--- ch8/scheduler.py
+++ ch8/scheduler.py
@@ -1,3 +1,19 @@
+import heapq
+import time
+
 from .task import Callback, Task
 
+class Scheduler:
+    def __init__(self) -> None:
+        self.tasks: list[Task] = []
+
+    def enter(
+        self,
+        after: int,
+        task: Callback,
+        delay: int = 0,
+        limit: int = 1,
+    ) -> None:
+        new_task = Task(after, task, delay, limit)
+        heapq.heappush(self.task, new_task)
 
