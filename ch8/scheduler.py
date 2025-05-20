c1b62bb464c7cb55200bfb4660cfc5e1adad6610
nethead21 <junivensaavedra@gmail.com>
2025-05-21 02:17:53 +0800

    Added run method to class Scheduler

--- ch8/scheduler.py
+++ ch8/scheduler.py
@@ -17,3 +17,13 @@ class Scheduler:
         new_task = Task(after, task, delay, limit)
         heapq.heappush(self.task, new_task)
 
+    def run(self) -> None:
+        current_time = 0
+        while self.tasks:
+            next_task = heapq.heappop(self.tasks)
+            if (delay := next_task.scheduled - current_time) > 0:
+                time.sleep(next_task.scheduled - current_time)
+            current_time = next_task.scheduled
+            next_task.callback(current_time)
+            if again := next_task.repeat(current_time):
+                heapq.heappush(self.tasks, again)
