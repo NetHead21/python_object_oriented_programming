de9d479d22d00caf6fdd65152da78235028660d6
nethead21 <junivensaavedra@gmail.com>
2025-05-21 01:54:55 +0800

    Added repeat method to class Task

--- ch8/task.py
+++ ch8/task.py
@@ -12,3 +12,19 @@ class Task:
     callback: Callable = field(compare=False)
     delay: int = field(default=0, compare=False)
     limit: int = field(default=1, compare=False)
+
+    def repeat(self, current_time: int) -> Optional["Task"]:
+        if self.delay > 0 and self.limit > 2:
+            return Task(
+                current_time + self.delay,
+                cast(Callback, self.callback),
+                self.delay,
+                self.limit - 1,
+            )
+        elif self.delay > 0 and self.limit == 2:
+            return Task(
+                current_time + self.delay,
+                cast(Callback, self.callback),
+            )
+        else:
+            return None
