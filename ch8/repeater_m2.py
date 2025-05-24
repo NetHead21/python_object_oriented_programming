aed013ac15015b321333c921c3193d0b5cd0dc38
nethead21 <junivensaavedra@gmail.com>
2025-05-24 21:12:37 +0800

    Added format_time to repeater_m2 class

--- ch8/repeater_m2.py
+++ ch8/repeater_m2.py
@@ -1,7 +1,16 @@
+from format_time import format_time
+
 class Repeater_2:
     def __init__(self) -> None:
         self.count = 0
 
-    def __call__(self, timer: float) -> none:
+    def __call__(self, timer: float) -> None:
         self.count += 1
         format_time(f"Called Four: {self.count}")
+
+
+rpt = Repeater_2()
+print(rpt(1))
+print(rpt(2))
+print(rpt(3))
+print(rpt(4))
