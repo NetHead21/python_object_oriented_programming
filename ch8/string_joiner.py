23d9a16aeb3eb13dabac3f4c92f3777f2d5db0a0
nethead21 <junivensaavedra@gmail.com>
2025-05-25 02:28:32 +0800

    Added __exit__ method to StringJoiner class.

--- ch8/string_joiner.py
+++ ch8/string_joiner.py
@@ -1,5 +1,15 @@
-from typing import Optional
+from typing import Optional, Type, Literal
 from types import TracebackType
 
 class StirngJoiner(list[str]):
+    def __enter__(self) -> "StringJoiner":
+        return self
 
+    def __exit__(
+        self,
+        exc_type: Optional[Type[BaseException]],
+        exc_val: Optional[BaseException],
+        exc_tb: Optional[TracebackType],
+    ) -> Literal[False]:
+        self.result = "".join(self)
+        return False
