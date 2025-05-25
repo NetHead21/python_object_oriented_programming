abe1092fa666612cf416cdabf5e19469ab1452b1
nethead21 <junivensaavedra@gmail.com>
2025-05-25 23:18:01 +0800

    Test joiner contextmanager

--- ch8/joiner.py
+++ ch8/joiner.py
@@ -1,2 +1,20 @@
 from contextlib import contextmanager
 from typing import Iterator
+
+from string_joiner_m2 import StringJoiner2
+
+
+@contextmanager
+def joiner(*args: any) -> Iterator[StringJoiner2]:
+    string_list = StringJoiner2(*args)
+    try:
+        yield string_list
+    finally:
+        string_list.result = "".join(string_list)
+
+
+with joiner("Hello") as join:
+    join.append(", ")
+    join.append("world!")
+
+print(join)
