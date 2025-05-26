8818cb39c73aeace6282b6c2aa5de33e91ab5855
nethead21 <junivensaavedra@gmail.com>
2025-05-26 22:43:17 +0800

    Test string_manipulation.py

--- ch9/string_manipulation.py
+++ ch9/string_manipulation.py
@@ -1,2 +1,13 @@
 a = "hello"
-
+b = "world"
+c = '''a multiple
+line string'''
+d = """More 
+multiple"""
+e = ("Three " "Strings "
+     "Together")
+print(f"{a}")
+print(f"{b}")
+print(f"{c}")
+print(f"{d}")
+print(f"{e}")
