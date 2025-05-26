e5e856621260173ffde46ec124c22e409f7091f1
nethead21 <junivensaavedra@gmail.com>
2025-05-26 23:05:00 +0800

    Added more string operations

--- ch9/string_operations.py
+++ ch9/string_operations.py
@@ -1,2 +1,13 @@
 s = "hello world"
 print(s.count("l"))
+print(s.find("l"))
+#print(s.rindex("m"))
+
+s2 = "hello world, how are you?"
+s3 = s2.split()
+print(f"{s3=}")
+
+print("#".join(s3))
+s2.replace(" ", "**")
+print(f"{s2=}")
+print(s2.partition(" "))
