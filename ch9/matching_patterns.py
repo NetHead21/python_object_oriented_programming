77b1d4c9fc778087bd9e8e6b0ef94cf80df0e8a5
nethead21 <junivensaavedra@gmail.com>
2025-05-29 00:51:34 +0800

    Added matchy function to matching_patterns.py

--- ch9/matching_patterns.py
+++ ch9/matching_patterns.py
@@ -1,4 +1,5 @@
 import re
+from typing import Patern, Match
 
 search_string = "hello world"
 pattern = r"hello world"
@@ -6,3 +7,10 @@ pattern = r"hello world"
 if match := re.match(pattern, search_string):
     print("regex matches")
     print(match)
+
+
+def matchy(pattern: Pattern[str], text: str) -> None:
+    if match := re.match(pattern, text):
+        print(f"{pattern=!r} matches at {match=!r}")
+    else:
+        print(f"{pattern=!r} not found in {text=!r}")
