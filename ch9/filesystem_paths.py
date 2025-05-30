17c2b6fa4d893b63108f6e77d4bb9aaf6f4cdda0
nethead21 <junivensaavedra@gmail.com>
2025-05-30 22:27:38 +0800

    Created path using Path module from Pathlib

--- ch9/filesystem_paths.py
+++ ch9/filesystem_paths.py
@@ -1,6 +1,12 @@
 import os.path
+from pathlib import Path
+
 
 path = os.path.abspath(
     os.sep.join(["", "Users", "Admin", "subdir", "subsubdir", "file.txt"])
 )
+print(path)
+
+
+path = Path("/Users") / "Admin" / "subdir" / "subsubdir" / "file.txt"
 print(path)
