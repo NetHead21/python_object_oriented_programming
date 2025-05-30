be232625a0bbfe3c972408523e81a2a1ea8eaf4b
nethead21 <junivensaavedra@gmail.com>
2025-05-30 22:30:37 +0800

    Build scan_python_1 function

--- ch9/scan_python.py
+++ ch9/scan_python.py
@@ -1,4 +1,12 @@
 from pathlib import Path
 from typing import Callable
 
+def scan_python_1(path: Path) -> int:
+    sloc = 0
+    with path.open() as source:
+        for line in source:
+            line = line.strip()
+            if line and not line.startswith("#"):
+                sloc += 1
+    return sloc
 
