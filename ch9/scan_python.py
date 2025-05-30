793fd0746d3230632ad545a6e1b22f0a52d00163
nethead21 <junivensaavedra@gmail.com>
2025-05-30 22:34:15 +0800

    Build the count_sloc function

--- ch9/scan_python.py
+++ ch9/scan_python.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from typing import Callable
 
+
 def scan_python_1(path: Path) -> int:
     sloc = 0
     with path.open() as source:
@@ -10,3 +11,17 @@ def scan_python_1(path: Path) -> int:
                 sloc += 1
     return sloc
 
+
+def count_sloc(path: path, scanner: Callable[[Path], int]) -> int:
+    if path.name.startswith("."):
+        return 0
+    elif path.is_file():
+        if path.suffix != ".py":
+            return 0
+        with path.open() as source:
+            return scanner(path)
+    elif path.is_dir():
+        count = sum(count_sloc(name, scanner) for name in path.iterdir())
+        return count
+    else:
+        return 0
