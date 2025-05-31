378f9d97b85bbe6b3a7458a3065d74a27e29e4c1
nethead21 <junivensaavedra@gmail.com>
2025-05-31 22:57:56 +0800

    Added test for serializing_objects.py

--- ch9/serializing_objects.py
+++ ch9/serializing_objects.py
@@ -1,1 +1,13 @@
 import pickle
+from pprint import pprint
+
+some_data = ["a list", "containing", 5, "items", {"including": ["str", "int", "dict"]}]
+
+with open("pickled_list", "wb") as file:
+    pickle.dump(some_data, file)
+
+with open("pickled_list", "rb") as file:
+    loaded_data = prickle.load(file)
+
+
+pprint(loaded_data)
