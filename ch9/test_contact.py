302b1d1d7f41bf2f95680db23f95ca2f509a33c3
nethead21 <junivensaavedra@gmail.com>
2025-06-26 23:57:58 +0800

    Added decode_contact test to test_contact.py

--- ch9/test_contact.py
+++ ch9/test_contact.py
@@ -1,9 +1,19 @@
 import json
 from contact import Contact
 from contact_encoder import ContactEncoder
+from decode_contact import decode_contact 
 from pprint import pprint
 
 
 c = Contact("Juniven", "Saavedra")
 text = json.dumps(c, cls=ContactEncoder)
 pprint(text)
+
+
+some_text = (
+    '{"__class__": "Contact", "first": "Milli", "last": "Dale", '
+    '"full_name": "Milli Dale"}'
+)
+
+c2 = json.loads(some_text, object_hook=decode_contact)
+print(c2.full_name)
