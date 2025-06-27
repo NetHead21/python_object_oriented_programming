50ae18c44f6219a4c578859269fc0ff2e3ddc36c
nethead21 <junivensaavedra@gmail.com>
2025-06-27 23:24:15 +0800

    Added CapitalIterator class in iterator_protocol.py

--- ch10/iterator_protocol.py
+++ ch10/iterator_protocol.py
@@ -7,3 +7,17 @@ class CapitalIterable(Iterable[str]):
 
     def __iter__(self) -> Iterator[str]:
         return CapitalIterator(self.string)
+
+
+class CapitalIterator(Iterator[str]):
+    def __init__(self, string: str) -> None:
+        self.words = [w.capitalize() for w in string.split()]
+        self.index = 0
+
+    def __next__(self) -> str:
+        if self.index == len(self.words):
+            raise StopIteration()
+
+        word = self.words[self.index]
+        self.index += 1
+        return word
