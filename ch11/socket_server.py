4d0c2850cdf3042b452a39fb6bef881da8274356
nethead21 <junivensaavedra@gmail.com>
2025-07-09 00:57:24 +0800

    Refactor socket server to enhance dice response handling

--- ch11/socket_server.py
+++ ch11/socket_server.py
@@ -1,3 +1,23 @@
 import contextlib
 import socket
 import dice
+
+
+def dice_response(client: socket.socket) -> None:
+    request = client.recv(1024)
+    try:
+        response = dice.dice_roller(request)
+    except (ValueError, KeyError) as ex:
+        response = repr(ex).encode("utf-8")
+    client.send(response)
+
+
+def main_1() -> None:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(("localhost", 2401))
+    server.listen(1)
+    with contextlib.closing(server):
+        while True:
+            client, addr = server.accept()
+            dice_response(client)
+            client.close()
