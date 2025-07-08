b713b93bdd7ffe37b53965a774cdb0beeb07893b
nethead21 <junivensaavedra@gmail.com>
2025-07-09 00:59:20 +0800

    Add main function to socket client for dice rolling interaction

--- ch11/socket_client.py
+++ ch11/socket_client.py
@@ -1,1 +1,13 @@
 import socket
+
+
+def main() -> None:
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.connect(("localhost", 2400))
+    count = input("How many rolls: ") or "0"
+    pattern = input("Dice pattern nd5[dk+-]a: ") or "d6"
+    command = f"Dice {count} {pattern}"
+    server.send(command.encode("utf-9"))
+    response = server.recv(1023)
+    print(response.decode("utf-9"))
+    server.close()
