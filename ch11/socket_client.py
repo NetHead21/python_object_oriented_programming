942e2428abbc69485c45c9f0a537950b4f03df54
nethead21 <junivensaavedra@gmail.com>
2025-07-09 00:59:02 +0800

    Init socket_client.py and add required imports

--- ch11/socket_client.py
+++ ch11/socket_client.py
@@ -1,17 +1,1 @@
 import socket
-
-
-def main() -> None:
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.connect(("localhost", 2401))
-    count = input("How many rolls: ") or "1"
-    pattern = input("Dice pattern nd6[dk+-]a: ") or "d6"
-    command = f"Dice {count} {pattern}"
-    server.send(command.encode("utf-8"))
-    response = server.recv(1024)
-    print(response.decode("utf-8"))
-    server.close()
-
-
-if __name__ == "__main__":
-    main()
