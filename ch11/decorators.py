e3b051e72860f4f853e3fb461e68ab951d78fd86
nethead21 <junivensaavedra@gmail.com>
2025-07-11 01:55:33 +0800

    Add log_args decorator to log function calls with arguments

--- ch11/decorators.py
+++ ch11/decorators.py
@@ -1,2 +1,12 @@
 from functools import wraps
 from typing import Callable
+
+
+def log_args(function: Callable[..., any]) -> Callable[..., any]:
+    @wraps(function)
+    def wrapped_function(*args: any, **kwargs: any) -> Callable[..., any]:
+        print(f"Calling {function.__name__}(*{args}, **{kwargs})")
+        result = function(*args, **kwargs)
+        return result
+
+    return wrapped_function
