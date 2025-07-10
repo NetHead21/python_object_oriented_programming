8920c0c752d24c48487d5007345dbb1e8ea2e587
nethead21 <junivensaavedra@gmail.com>
2025-07-11 00:48:42 +0800

    Add LogRoller class to handle dice rolling requests with remote address logging

--- ch11/dice_server.py
+++ ch11/dice_server.py
@@ -1,1 +1,16 @@
 from typing import Callable
+
+Address = tuple[str, int]
+
+
+class LogRoller:
+    def __init__(self, dice: Callable[[bytes], bytes], remote_addr: Address) -> None:
+        self.dice_roller = dice
+        self.remote_addr = remote_addr
+
+    def __call__(self, request: bytes) -> bytes:
+        print(f"Receiving {request!r} from {self.remote_addr}")
+        dice_roller = self.dice_roller
+        response = dice_roller(request)
+        print(f"Sending {request!r} to {self.remote_addr}")
+        return response
