977f9e49cb20983924f06ff16c3c14c2af24b51c
nethead21 <junivensaavedra@gmail.com>
2025-10-16 09:43:02 +0800

    Add average function to compute the average of a list, ignoring None values

--- ch13/tests/test_averages.py
+++ ch13/tests/test_averages.py
@@ -1,2 +1,10 @@
 import unittest
 from typing import Optional
+
+
+def average(seq: list[Optional[float]]) -> float:
+    """Compute the average of a list of numbers, ignoring None values."""
+    filtered_seq = [x for x in seq if x is not None]
+    if not filtered_seq:
+        raise ValueError("Cannot compute average of empty sequence")
+    return sum(filtered_seq) / len(filtered_seq)
