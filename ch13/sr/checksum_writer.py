cf0385b20025e4d9febfa65f2fe1fa18a3eceb42
nethead21 <junivensaavedra@gmail.com>
2025-10-17 23:52:33 +0800

    Implement checksum function to calculate and store SHA-256 checksums

--- ch13/sr/checksum_writer.py
+++ ch13/sr/checksum_writer.py
@@ -1,3 +1,11 @@
 import tarfile
 from pathlib import Path
 import hashlib
+
+
+def checksum(source: Path, checksum_path: Path) -> None:
+    if checksum_path.exists():
+        backup = checksum_path.with_stem(f"(old) {checksum_path.stem}")
+        backup.write_text(checksum_path.read_text())
+    checksum = hashlib.sha256(source.read_bytes())
+    checksum_path.write_text(f"{source.name} {checksum.hexdigest()}\n")
