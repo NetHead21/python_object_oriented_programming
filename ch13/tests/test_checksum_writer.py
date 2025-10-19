b49805feda1cf5e553f6eb0dd8ba5b28c45f680e
nethead21 <junivensaavedra@gmail.com>
2025-10-20 00:08:01 +0800

    Add test for FileChecksum to validate different content produces different hashes

--- ch13/tests/test_checksum_writer.py
+++ ch13/tests/test_checksum_writer.py
@@ -355,3 +355,9 @@ class TestFileChecksumEdgeCases:
         file1 = tmp_path / "file1.txt"
         file1.write_bytes(b"Content A")
         fc1 = checksum_writer.FileChecksum(file1)
+
+        file2 = tmp_path / "file2.txt"
+        file2.write_bytes(b"Content B")
+        fc2 = checksum_writer.FileChecksum(file2)
+
+        assert fc1.checksum.hexdigest() != fc2.checksum.hexdigest()
