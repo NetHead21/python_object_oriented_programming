9f067611bd330494c651d3fb8ea8bac64c3aae8c
nethead21 <junivensaavedra@gmail.com>
2025-11-18 00:00:47 +0800

    Reintroduce sleepers function in async_1.py for task creation and management

--- ch14/src/async_1.py
+++ ch14/src/async_1.py
@@ -7,3 +7,10 @@ async def random_sleep(counter: float) -> None:
     print(f"{counter} sleeps for {delay:.2f} seconds")
     await asyncio.sleep(delay)
     print(f"{counter} awakens, refreshed")
+
+
+async def sleepers(how_many: int = 5) -> None:
+    print(f"Creating {how_many} tasks")
+    tasks = [asyncio.create_task(random_sleep(i)) for i in range(how_many)]
+    print(f"Waiting for {how_many} tasks")
+    await asyncio.gather(*tasks)
