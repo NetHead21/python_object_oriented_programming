2e083364ee22dd98798e39d6a4a7c7baf0d03fbf
nethead21 <junivensaavedra@gmail.com>
2026-01-16 23:54:03 +0800

    Add test for rejection of user ID shorter than 3 characters in TestBookingInputServiceCollectUserId

--- ch11/test_metaclass_orm.py
+++ ch11/test_metaclass_orm.py
@@ -393,3 +393,7 @@ class TestBookingInputServiceCollectUserId(unittest.TestCase):
 
         self.assertEqual(result, "user123")
         self.assertEqual(mock_input.call_count, 2)
+
+    @patch("business_logic.services.booking_input_service.get_user_input")
+    def test_collect_user_id_too_short(self, mock_input):
+        """Test rejection of user ID shorter than 3 characters."""
